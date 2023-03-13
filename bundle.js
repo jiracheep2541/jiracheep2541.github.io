387f742ba54e3869a3d28777ecb27b198cb4d205
jiracheep2541 <50222920+jiracheep2541@users.noreply.github.com>
2023-03-13 16:41:11 +0700

    Add files via upload

--- bundle.js
+++ bundle.js
@@ -115893,20 +115893,44 @@ function CreateViewer(container) {
     return viewer;
 }
 
+
+
 const container = document.getElementById('viewer-container');
 let viewer = CreateViewer(container);
+
+// viewer.IFC.loader.ifcManager.applyWebIfcConfig({
+//     USE_FAST_BOOLS: true,
+//     COORDINATE_TO_ORIGIN: true
+// });
+
+
 // console.log('viewer', viewer.axes.dispose())
 
 const input = document.getElementById("file-input");
 
+const handleKeyDown = async(event) => {
+    if (event.code === 'Escape') {
+        try {
+
+            viewer.clipper.deleteAllPlanes();
+            viewer.clipper.planes = [];
+        } catch {}
+
+    }
+};
+
+
 window.onmousemove = () => viewer.IFC.selector.prePickIfcItem();
 // Select items and log properties
+window.onkeydown = handleKeyDown;
+
 window.ondblclick = async() => {
-    const item = await viewer.IFC.selector.pickIfcItem(true);
-    if (item.modelID === undefined || item.id === undefined) return;
-    console.log(await viewer.IFC.getProperties(item.modelID, item.id, true));
+
+    if (viewer.clipper.active) {
+        viewer.clipper.createPlane();
+    }
+
 };
-viewer.clipper.active = true;
 
 input.addEventListener("change",
 
@@ -115927,18 +115951,20 @@ async function loadIfc(url) {
     const model = await viewer.IFC.loadIfcUrl(url);
     viewer.shadowDropper.renderShadow(model.modelID);
 
+    viewer.clipper.toggle();
+
     // viewer.axes.dispose();
     // viewer.grid.dispose();
 }
 
 loadIfc('models/Walkers.ifc');
 
-window.onkeydown = (event) => {
-    if (event.code === 'KeyP') {
-        viewer.clipper.createPlane();
-    } else if (event.code === 'KeyO') {
-        viewer.clipper.deletePlane();
-    } else if (event.code === 'Escape') {
-        viewer.IFC.selector.unpickIfcItems();
-    }
-};
+// window.onkeydown = (event) => {
+//     if (event.code === 'KeyP') {
+//         viewer.clipper.createPlane();
+//     } else if (event.code === 'KeyO') {
+//         viewer.clipper.deletePlane();
+//     } else if (event.code === 'Escape') {
+//         viewer.IFC.selector.unpickIfcItems();
+//     }
+// }
